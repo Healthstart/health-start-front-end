c0dcc65df5d92cd166ba1b98d3ac3ec973f887a2
Sunwoo Ha <tjsdn811@gmail.com>
2021-07-07 16:52:37 +0900

    feat: create Button Atomics

---
+++ src/Atomic/Buttons/index.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const RowButton = styled.button`
+    cursor: pointer;
+
+    width: ${(props) => (props.w ? props.w : 120)}px;
+    height: ${(props) => (props.h ? props.h : 50)}px;
+
+    background-color: ${(props) => (props.bgColor ? props.bgColor : '#fa2929')};
+    color: ${(props) => (props.color ? props.color : '#ffffff')};
+    font-size: ${(props) => (props.fSize ? props.fSize : 20)}px;
+    font-weight: ${(props) => (props.fWei ? props.fWei : 600)};
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    border: none;
+    border-radius: ${(props) => (props.bdr ? props.bdr : 15)}px;
+`;
+
+export const CircleButton = styled(RowButton)`
+    width: ${(props) => (props.w ? props.w : 50)}px;
+    height: ${(props) => (props.h ? props.h : 50)}px;
+
+    border-radius: 50%;
+`;
+
+export default RowButton;
