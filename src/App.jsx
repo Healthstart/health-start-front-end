9c2c0e486342f98fda54cb488f456a207244912f
Sunwoo Ha <tjsdn811@gmail.com>
2021-07-07 16:52:17 +0900

    test: style test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Heading1, Heading2, Heading3 } from './Atomic/Heading';
+import { RowButton, CircleButton } from './Atomic/Buttons';
 
 const App = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Heading1>제목1 Title1</Heading1>
+            <Heading2>제목2 Title2</Heading2>
+            <Heading3>제목3 Title3</Heading3>
+
+            <RowButton>Row 버튼</RowButton>
+            <CircleButton>X</CircleButton>
+        </div>
+    );
 };
 
 export default App;
