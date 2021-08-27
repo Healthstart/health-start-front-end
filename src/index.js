cdac40a35d1762e2554d95a74ddee195032b0b38
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-08-27 11:25:58 +0900

    feat: Toast 메세지 기능 삽입

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './style/global.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.render(
     <React.StrictMode>
         <ChakraProvider>
             <App />
+
+            <Toaster />
         </ChakraProvider>
     </React.StrictMode>,
     document.getElementById('root')
