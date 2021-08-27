a84adc11f07eaefd7ef7ee788722d065c17bd515
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-08-27 10:44:09 +0900

    feat: Form 작성을 위한 Form Atomic 구현

---
+++ src/Atomic/Form/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Heading3 } from '../Heading';
+
+export const InputForm = styled.input`
+    width: ${(props) => (props.w ? props.w : 300)}px;
+    height: ${(props) => (props.h ? props.h : 45)}px;
+    font-size: 1.2rem;
+    padding: 0 1.2rem;
+    color: #565656;
+    border: none;
+    border-radius: 20rem;
+    background: #efefef;
+    box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px #ffffff;
+    outline: none;
+    transition: all 0.3s ease-in-out;
+
+    &:focus {
+        box-shadow: inset 1px 1px 2px #babecc, inset -1px -1px 2px #ffffff;
+    }
+    &::placeholder {
+        text-align: center;
+    }
+`;
+
+const LabelContainer = styled.label`
+    display: block;
+    margin-bottom: 0.3rem;
+`;
+
+export const LabelForm = ({ children }) => {
+    return (
+        <LabelContainer>
+            <Heading3 fSize={22} color={'#565656'}>
+                {children}
+            </Heading3>
+        </LabelContainer>
+    );
+};
