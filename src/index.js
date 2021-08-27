ebc045e1a09b29231d90b6a522e6369fc798a9f9
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-08-27 11:44:44 +0900

    fix: global css 적용을 막는 chakraProvider 임시제거

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/global.css';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
 import { Toaster } from 'react-hot-toast';
 
 ReactDOM.render(
     <React.StrictMode>
-        <ChakraProvider>
-            <App />
+        <App />
 
-            <Toaster />
-        </ChakraProvider>
+        <Toaster />
     </React.StrictMode>,
     document.getElementById('root')
 );
