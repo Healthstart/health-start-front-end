be72cc6a5ebc5738698a40411ece54fd8f0339e3
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-08-27 11:25:37 +0900

    feat: 로그인 배너 이미지 테스트

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -4,6 +4,7 @@ import Login from '../Compnents/Login';
 
 const Container = styled.div`
     display: flex;
+    position: relative;
     align-items: center;
     width: 1920px;
     height: 100vh;
@@ -11,9 +12,22 @@ const Container = styled.div`
     box-shadow: 15px 15px 35px #807c7c, -15px -15px 35px #ffffff;
 `;
 
+const BackgroundImage = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    font-size: 62px;
+`;
+
 const LoginPage = () => {
     return (
         <Container>
+            <BackgroundImage>---------------IMAGE--------------</BackgroundImage>
             <Login />
         </Container>
     );
