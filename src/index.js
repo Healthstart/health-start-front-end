5e546911dafa9108c9f6b33ad8ad53dcfd869d84
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-08-27 10:54:00 +0900

    pull: pull devleop

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/global.css';
 import App from './App';
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
     <React.StrictMode>
         <ChakraProvider>
-          <App />
+            <App />
         </ChakraProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
-    
