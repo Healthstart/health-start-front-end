cb6fa46d33952dc8e56e6f5b0345b26bc1ca29a3
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-09-10 13:36:59 +0900

    feat: axios 모듈 헤더 설정

--- src/Api/index.js
+++ src/Api/index.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const Api = axios.create({
     baseURL: process.env.REACT_APP_BACK_HOST,
+    withCredentials: false,
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer `,
+      },
 });
 
 export default Api;
