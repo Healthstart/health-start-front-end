39732be67ebdbf541544b41ca5f3d2ffefeb70b1
Sunwoo Ha <tjsdn811@gmail.com>
2021-09-10 10:41:37 +0900

    feat: 임시페이지를 대시보드에 맞게 수정

--- src/Pages/TempPage.jsx
+++ src/Pages/TempPage.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Heading1, Heading3 } from '../Atomic/Heading';
+import { Page } from "../Atomic/Background";
+import { Heading1, Heading2, Heading3 } from '../Atomic/Heading';
 
-const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
 
 const Content = styled.div`
-    width: 600px;
+    width: 100%;
     height: 300px;
     text-align: center;
+
+    margin-top: 350px;
 `;
 
 const Back = styled(Link)`
@@ -23,15 +19,15 @@ const Back = styled(Link)`
 
 const TempPage = () => {
     return (
-        <Container>
+        <Page>
             <Content>
-                <Heading1>아직 미구현된 페이지입니다.</Heading1>
+                <Heading2>아직 미구현된 페이지입니다.</Heading2>
                 <Heading3>개발 진행 중</Heading3>
                 <Heading3>
                     <Back to="/">뒤로가기</Back>
                 </Heading3>
             </Content>
-        </Container>
+        </Page>
     );
 };
 
