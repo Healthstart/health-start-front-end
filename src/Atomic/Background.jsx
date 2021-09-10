48a97fc63b4ba6cdddb7e9c94578590864b67d9b
Sunwoo Ha <tjsdn811@gmail.com>
2021-09-10 10:40:59 +0900

    feat: 대시보드를 위한 컴포넌트 분리

---
+++ src/Atomic/Background.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+    position: fixed;
+
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #cbd1df;
+`;
+
+export const Dashboard = styled.div`
+    position: relative;
+
+    width: 88%;
+    height: 90%;
+    margin: 0 auto;
+
+    background-color: #f6f7f9;
+    border-radius: 35px;
+`;
+
+
+export const Page = styled.div`
+    position: absolute;
+
+    left: 350px;
+    top: 0;
+
+    width: 1330px;
+    height: 820px;
+`;
+   
