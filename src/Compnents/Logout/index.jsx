62a799fb01ff9d5458367181951a0ede91fe97a5
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-09-24 11:15:10 +0900

    feat: 로그아웃 임시 페이지 작성

--- src/Compnents/Logout/index.jsx
+++ src/Compnents/Logout/index.jsx
@@ -1,13 +1,42 @@
 import React, { useEffect } from 'react';
 import { removeToken } from '../../Utils/managesToken';
-import PermissionPage from '../../Pages/PermissionPage';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { Heading1, Heading3 } from '../../Atomic/Heading';
 
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Content = styled.div`
+    width: 600px;
+    height: 300px;
+    text-align: center;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: underline;
+`;
 const Logout = () => {
     useEffect(() => {
         removeToken();
     });
 
-    return <PermissionPage />;
+    return (
+        <Container>
+            <Content>
+                <Heading1>로그아웃</Heading1>
+                <Heading3>로그아웃이 성공적으로 완료되었습니다.</Heading3>
+                <Heading3>
+                    <StyledLink to="/login">로그인 하러가기</StyledLink>
+                </Heading3>
+            </Content>
+        </Container>
+    );
 };
 
 export default Logout;
