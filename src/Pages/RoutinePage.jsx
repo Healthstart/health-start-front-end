fba2aeea46fc5ade0bbe8f34f16a21d1ae860749
unknown <tjsdn811@gmail.com>
2021-09-24 11:19:47 +0900

    feat: Add RoutinePage

---
+++ src/Pages/RoutinePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const RoutinePage = () => {
+    return(
+        <div>
+            test
+        </div>
+    )
+}
+
+export default RoutinePage;
