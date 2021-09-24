811b022c76b1519d6108addbbf5cc6b3c238b402
unknown <tjsdn811@gmail.com>
2021-09-24 11:18:44 +0900

    fix: design fix

--- src/Atomic/Background.jsx
+++ src/Atomic/Background.jsx
@@ -1,4 +1,17 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const BGgradient = keyframes`
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+`;
+
 
 export const Background = styled.div`
     position: fixed;
@@ -9,8 +22,13 @@ export const Background = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #cbd1df;
+    
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	
+    animation: ${BGgradient} 15s ease infinite;
 `;
+
 
 export const Dashboard = styled.div`
     position: relative;
