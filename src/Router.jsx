c3418a4c62f47e3bbd7ea080e2b290e809867ac9
Twince <tjsdn811@gmail.com>
2021-09-28 11:55:43 +0900

    feat: 라우팅 변경

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,6 +5,7 @@ import ProfilePage from './Pages/ProfilePage';
 import TempPage from './Pages/TempPage';
 import Sidemenu from './Compnents/Sidemenu/Sidemenu';
 import PermissionRoute from './Compnents/Route/PermissionRoute';
+import RoutinePage from './Pages/RoutinePage';
 
 const Router = ({ match }) => {
     return (
@@ -13,7 +14,7 @@ const Router = ({ match }) => {
                 <Sidemenu match={match} />
                 <Switch>
                     <PermissionRoute path={`${match.path}/`} exact component={ProfilePage} />
-                    <PermissionRoute path={`${match.path}/exercise`} component={TempPage} />
+                    <PermissionRoute path={`${match.path}/exercise`} component={RoutinePage} />
                     <PermissionRoute path={`${match.path}/temp`} component={TempPage} />
                 </Switch>
             </Dashboard>
