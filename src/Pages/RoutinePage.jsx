3e12a580f75b215a60e64e9141d7e61c32b337b3
Sunwoo Ha <tjsdn811@gmail.com>
2021-10-08 10:27:44 +0900

    feat: 타이머 CSS 스타일링 + 상태값에 따라 스톱워치 시작 및 종료 구현

--- src/Pages/RoutinePage.jsx
+++ src/Pages/RoutinePage.jsx
@@ -31,8 +31,8 @@ const ContentWrap = styled.div`
     width: 65vw;
     height: 600px;
 
+    margin-top: 40px;
     display: flex;
-    background-color: #8d8d8d;
 `;
 
 const TimerSection = styled.div`
@@ -40,46 +40,99 @@ const TimerSection = styled.div`
     height: 95%;
 
     margin: 0 auto;
-    background-color: #db8484;
-    margin-top:10px;
+    margin-top: 10px;
 
-    border-radius: 10px;
+    display: flex;
 `;
+
+const TimerBox = styled.div`
+    width: 150px;
+    height: 250px;
+
+    align-self: center;
+    margin: 0 auto;
+`;
+
+const TimerText = styled.h3`
+    font-size: 33px;
+    color: #5f5f5f;
+    margin-top: -12px;
+
+    text-align: center;
+`;
+
+const TimerButton = styled.button`
+    cursor: pointer;
+
+    display: block;
+
+    width: 80px;
+    height: 35px;
+
+    border-radius: 30px;
+    border: 1px solid #2672ff;
+    background-color: none;
+
+    color: #2672ff;
+    font-weight: 700;
+    line-height: 35px;
+
+    margin: 0 auto;
+    margin-top: 10px;
+
+    transition: 0.2s;
+    &:hover {
+        background-color: #2672ff;
+        color: white;
+    }
+    &:active {
+        transform: scale(1.03);
+    }
+`;
+
 const RoutineSection = styled.div`
     width: 70%;
     height: 95%;
 
     background-color: #3e6df0;
-    margin-top:10px;
+    margin-top: 10px;
 
-    border-radius: 10px
+    border-radius: 10px;
 `;
 
 const TimerIcon = styled.img`
     width: 110px;
-    opacity: 0.8;
+    opacity: 0.6;
+
+    display: block;
+    margin: 0 auto;
 `;
 
 const RoutinePage = ({ mm, ss }) => {
     const [minutes, setMinutes] = useState(parseInt(2));
     const [seconds, setSeconds] = useState(parseInt(0));
+    const [timerState, setTimerState] = useState(false);
 
     useEffect(() => {
         console.log('마ㅏ');
-        const CountDown = setInterval(() => {
-            if (seconds > 0) {
-                setSeconds(parseInt(seconds) - 1);
-            }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    clearInterval(CountDown);
-                } else {
-                    setMinutes(parseInt(minutes) - 1);
-                    setSeconds(59);
+        if (timerState == false) {
+            console.log('타이머 안 움직임');
+        } else {
+            const CountDown = setInterval(() => {
+                if (seconds > 0) {
+                    setSeconds(parseInt(seconds) - 1);
                 }
-            }
-        }, 1000);
-        return () => clearInterval(CountDown);
+                if (seconds === 0) {
+                    if (minutes === 0) {
+                        clearInterval(CountDown);
+                    } else {
+                        setMinutes(parseInt(minutes) - 1);
+                        setSeconds(59);
+                    }
+                }
+            }, 1000);
+            return () => clearInterval(CountDown);
+        }
     }, [minutes, seconds]);
 
     return (
@@ -90,18 +143,18 @@ const RoutinePage = ({ mm, ss }) => {
 
             <ContentWrap>
                 <TimerSection>
-                    <TimerIcon src={RoutinTImerIcon} />
-                    <h4>
-                        {minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-                        
-                    </h4>
-                    <h5>UseEffect 커스텀 타이머 적용</h5>
+                    <TimerBox>
+                        <TimerIcon src={RoutinTImerIcon} />
+                        <TimerText>
+                            {minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+                        </TimerText>
+                        <TimerButton>완료</TimerButton>
+                    </TimerBox>
                 </TimerSection>
                 <RoutineSection>
                     <h5>Current and Next Routin Page 작업 섹션 Animte.css / motoin framer dependency</h5>
                 </RoutineSection>
             </ContentWrap>
-
         </Page>
     );
 };
