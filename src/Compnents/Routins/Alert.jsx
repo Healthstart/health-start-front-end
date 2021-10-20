e22783ced7ec74e72c6bcf55c28e7fccb61f7270
ArKc1009 <arkc1009@gmail.com>
2021-10-21 07:01:09 +0900

    Update Alert.jsx

--- src/Compnents/Routins/Alert.jsx
+++ src/Compnents/Routins/Alert.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { motion } from 'framer-motion';
 import { RoutinButton } from '../../Atomic/Buttons';
+import { Heading2 } from '../../Atomic/Heading';
 
 const Container = styled(motion.div)`
     position: absolute;
@@ -24,7 +25,6 @@ const Content = styled(motion.div)`
     z-index: 30;
 
     width: 600px;
-    height: 400px;
 
     background-color: #ebebeb;
     border-radius: 20px;
@@ -34,6 +34,14 @@ const Content = styled(motion.div)`
     transform: translate(-50%, -50%);
 `;
 
+const StartAlertContent = styled(Content)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2rem;
+    text-align: center;
+`;
+
 export const StartAlert = ({ isSelectState, selectedRoutinState }) => {
     const [isSelect, setIsSelect] = isSelectState;
     const [isRealSelect, setIsRealSelect] = useState(false);
@@ -41,7 +49,7 @@ export const StartAlert = ({ isSelectState, selectedRoutinState }) => {
 
     return (
         <Container initial={{ opacity: 0 }} animate={{ opacity: 1, display: isRealSelect ? 'none' : 'block' }}>
-            <Content
+            <StartAlertContent
                 initial={{ y: -1200, x: -300 }}
                 animate={{ y: isRealSelect ? -1000 : isSelect ? -180 : 0, x: -300 }}
                 transition={{
@@ -50,14 +58,20 @@ export const StartAlert = ({ isSelectState, selectedRoutinState }) => {
                     damping: 30,
                 }}
             >
+                <Heading2 style={{ marginBottom: '2vh' }}>선택하신 루틴이 맞는지 확인해주세요.</Heading2>
                 {selectedRoutin.content.map((x, i) => (
-                    <div key={i}>
+                    <div key={i} style={{ marginBottom: '3vh' }}>
                         <h3>{x.name}</h3>
+                        <p>
+                            {x.set}세트 / {x.count}회
+                        </p>
                     </div>
                 ))}
-                <RoutinButton onClick={() => setIsRealSelect((prevState) => true)}>완료</RoutinButton>
-                <RoutinButton onClick={() => setIsSelect((prevState) => false)}>돌아가기</RoutinButton>
-            </Content>
+                <div style={{ width: '200px', display: 'flex', justifyContent: 'center' }}>
+                    <RoutinButton onClick={() => setIsRealSelect((prevState) => true)}>완료</RoutinButton>
+                    <RoutinButton onClick={() => setIsSelect((prevState) => false)}>돌아가기</RoutinButton>
+                </div>
+            </StartAlertContent>
         </Container>
     );
 };
