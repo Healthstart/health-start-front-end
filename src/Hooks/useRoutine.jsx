01f2717ebfb1bc9df54ca9ec0e740124bd692584
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-10-28 13:29:01 +0900

    refactor: 본인 루틴 미리보기를 Context화

---
+++ src/Hooks/useRoutine.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useCallback, useContext, useEffect, useState } from 'react';
+import Api from '../Api';
+
+const Context = createContext(null);
+
+export const RoutineProvider = ({ children }) => {
+    const [routine, setRoutine] = useState(null);
+
+    const fetchData = useCallback(async () => {
+        const data = await Api.get('/lutin/preview');
+        if (!data) return;
+
+        setRoutine((prevState) => data.data.data);
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    return <Context.Provider value={routine}>{children}</Context.Provider>;
+};
+
+const useRoutine = () => useContext(Context);
+
+export default useRoutine;
--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import App from './App';
 import { Toaster } from 'react-hot-toast';
 import dotenv from 'dotenv';
 import { ProfileProvider } from './Hooks/useProfile';
+import { RoutineProvider } from './Hooks/useRoutine';
 
 dotenv.config();
 
 ReactDOM.render(
     <React.StrictMode>
         <ProfileProvider>
-            <App />
+            <RoutineProvider>
+                <App />
 
-            <Toaster />
+                <Toaster />
+            </RoutineProvider>
         </ProfileProvider>
     </React.StrictMode>,
     document.getElementById('root')
